0dfcd8b700b782c58edf1b30fdc8751cd0b8f06a
Sidnei Farias <sidneijr.77@gmail.com>
2023-11-16 11:13:46 -0300

    Inclui um loader, para o loading de cada página filha em 'Weapons'

--- src/app/weapons/loading.tsx
+++ src/app/weapons/loading.tsx
@@ -1,3 +1,15 @@
+import Image from 'next/image'
+
 export default function WeaponsLoading() {
-  return <h1>Loading...</h1>
+  return (
+    <section className="min-h-[100vh] flex flex-1 justify-center items-center">
+      <Image
+        src={'/loader.svg'}
+        alt="Loader"
+        width={120}
+        height={120}
+        quality={100}
+      />
+    </section>
+  )
 }
