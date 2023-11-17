387f724f82da3774eb4fd7a566b7717a2caa3f7e
Sidnei Farias <sidneijr.77@gmail.com>
2023-11-17 18:41:11 -0300

    Inclui Framer Motion, Select e ProgressBar

---
+++ src/components/motion-wrapper.tsx
@@ -0,0 +1,35 @@
+'use client'
+
+import { ReactNode } from 'react'
+
+import { motion } from 'framer-motion'
+
+interface IMotionWrapper {
+  children: ReactNode
+  initial: any
+  animate: any
+  transition: any
+  exit: any
+  style: any
+}
+
+export const MotionWrapper = ({
+  children,
+  initial,
+  animate,
+  transition,
+  exit,
+  style,
+}: IMotionWrapper) => {
+  return (
+    <motion.div
+      initial={initial}
+      animate={animate}
+      transition={transition}
+      exit={exit}
+      style={style}
+    >
+      {children}
+    </motion.div>
+  )
+}
