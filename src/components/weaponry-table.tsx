9530b1a6ac156c15e727c110a81d700b8bd3de03
Sidnei Farias <sidneijr.77@gmail.com>
2023-11-17 18:41:56 -0300

    Inclui Framer Motion em 'weaponry-table'

--- src/components/weaponry-table.tsx
+++ src/components/weaponry-table.tsx
@@ -5,6 +5,7 @@ import data from '../json/weapons.json'
 
 import { v4 as uuidv4 } from 'uuid'
 
+import { MotionWrapper } from './motion-wrapper'
 import {
   Table,
   TableBody,
@@ -69,25 +70,33 @@ export const WeaponryTable = () => {
   })
 
   return (
-    <Table className="max-w-[100rem] mx-auto">
-      <TableCaption className="text-3xl text-white font-bold mb-4 caption-top">
-        Firearms available
-      </TableCaption>
+    <MotionWrapper
+      initial={{ y: '25%', opacity: 0 }}
+      animate={{ y: '0%', opacity: 1 }}
+      transition={{ duration: 0.75, ease: [0.33, 1, 0.68, 1] }}
+      exit={{ y: '-25%', opacity: 0 }}
+      style={{ width: '100%', margin: '0 auto' }}
+    >
+      <Table className="max-w-[100rem] mx-auto">
+        <TableCaption className="text-3xl text-white font-bold mb-4 caption-top">
+          Firearms available
+        </TableCaption>
 
-      <TableHeader>
-        <TableRow>
-          <TableHead>Name</TableHead>
-          <TableHead>Damage</TableHead>
-          <TableHead>Fire rate</TableHead>
-          <TableHead>Capacity</TableHead>
-          <TableHead>Name</TableHead>
-          <TableHead>Firing Mode</TableHead>
-          <TableHead>Reload Time</TableHead>
-          <TableHead>Draw Speed</TableHead>
-        </TableRow>
-      </TableHeader>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Name</TableHead>
+            <TableHead>Damage</TableHead>
+            <TableHead>Fire rate</TableHead>
+            <TableHead>Capacity</TableHead>
+            <TableHead>Name</TableHead>
+            <TableHead>Firing Mode</TableHead>
+            <TableHead>Reload Time</TableHead>
+            <TableHead>Draw Speed</TableHead>
+          </TableRow>
+        </TableHeader>
 
-      <TableBody>{weaponry}</TableBody>
-    </Table>
+        <TableBody>{weaponry}</TableBody>
+      </Table>
+    </MotionWrapper>
   )
 }
