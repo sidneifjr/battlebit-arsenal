c9120946a04247235dac0fac8cc25dd94c2563ad
Sidnei Farias <sidneijr.77@gmail.com>
2023-11-30 09:32:28 -0300

    refactor(weaponry-list): melhora legibilidade

--- src/components/weaponry-list.tsx
+++ src/components/weaponry-list.tsx
@@ -29,17 +29,14 @@ import { Separator } from './ui/separator'
 
 interface WeaponInfo {
   id: number
-  pageName: string
   name: string
-  icon: string
+  pageName: string
   category: string
+  icon: string
+  image: string
   damage: number
   lightArmorDamage: number
   heavyArmorDamage: number
-  capacity: number
-  fireRate: number
-  reloadTime: number
-  drawSpeed: number
   verticalRecoil: number
   horizontalRecoil: number
   firstShotKick: number
@@ -50,11 +47,21 @@ interface WeaponInfo {
   control: number
   aimDownTime: number
   runningSpeed: number
+  capacity: number
+  firingMode: string[]
+  fireRate: number
+  reloadTime: number
+  drawSpeed: number
+  rangeData?: number[]
   boltActionSpeed?: number
+  map(
+    arg0: (weapon: any) => import('react').JSX.Element
+  ): import('react').ReactNode
+  toSorted(compareByName: (a: any, b: any) => any): WeaponInfo
 }
 
 export const WeaponryList = () => {
-  const [weapons, setWeapons] = useState(data.weapons)
+  const [weapons, setWeapons] = useState<WeaponInfo>(data.weapons)
 
   const handleSelect = (value: string) => {
     switch (value) {
@@ -110,138 +117,6 @@ export const WeaponryList = () => {
     }
   }
 
-  const weaponCards = weapons.map((weapon) => {
-    const {
-      id,
-      pageName,
-      name,
-      icon,
-      category,
-      damage,
-      fireRate,
-      capacity,
-      firingMode,
-      reloadTime,
-      verticalRecoil,
-      horizontalRecoil,
-      firstShotKick,
-      velocity,
-      accuracy,
-      soundSpread,
-      muzzleFlashScale,
-      control,
-      aimDownTime,
-      runningSpeed,
-    } = weapon
-
-    const adjustedWeaponPageName = pageName.replace(/\s/g, '-').toLowerCase()
-
-    return (
-      <Card key={id} className="flex-[25%]">
-        <CardHeader className="relative">
-          <div className="flex gap-4">
-            <Link href={`/weapons/${adjustedWeaponPageName}`}>
-              <Image
-                src={icon}
-                alt={name}
-                width={200}
-                height={100}
-                quality={100}
-              />
-            </Link>
-
-            <div className="flex flex-col gap-0.5 flex-1">
-              <CardTitle>{name}</CardTitle>
-
-              <CardDescription>{category}</CardDescription>
-
-              <CardDescription className="flex">
-                {firingMode.join(', ')}
-              </CardDescription>
-
-              <div className="flex gap-4">
-                <Link
-                  className="max-w-fit mt-2 inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-cyan-500 text-primary-foreground shadow hover:bg-cyan-500/80 h-9 px-4 py-2"
-                  href={`/weapons/${adjustedWeaponPageName}`}
-                >
-                  Customize
-                </Link>
-
-                <Button className="max-w-fit mt-2 inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground shadow hover:bg-primary/90 h-9 px-4 py-2">
-                  Add to Loadout
-                </Button>
-              </div>
-            </div>
-          </div>
-
-          <Separator className="absolute right-0 bottom-0 left-0 bg-white" />
-        </CardHeader>
-
-        <CardContent className="pt-4 flex gap-2 justify-between">
-          <div className="flex flex-col gap-1">
-            <p className="text-sm">
-              <strong>Damage:</strong> {damage}
-            </p>
-
-            <p className="text-sm">
-              <strong>Fire Rate:</strong> {fireRate}
-            </p>
-
-            <p className="text-sm">
-              <strong>Vertical recoil:</strong> {verticalRecoil}
-            </p>
-
-            <p className="text-sm">
-              <strong>Horizontal recoil:</strong> {horizontalRecoil}
-            </p>
-
-            <p className="text-sm">
-              <strong>First shot kick:</strong> {firstShotKick}
-            </p>
-
-            <p className="text-sm">
-              <strong>Capacity:</strong> {capacity}
-            </p>
-
-            <p className="text-sm">
-              <strong>Reload time:</strong> {reloadTime}
-            </p>
-          </div>
-
-          <div className="flex flex-col gap-1">
-            <p className="text-sm">
-              <strong>Velocity:</strong> {velocity}
-            </p>
-
-            <p className="text-sm">
-              <strong>Accuracy:</strong> {accuracy}
-            </p>
-
-            <p className="text-sm">
-              <strong>Sound Spread:</strong> {soundSpread}
-            </p>
-
-            <p className="text-sm">
-              <strong>Muzzle flash scale:</strong> {muzzleFlashScale}
-            </p>
-
-            <p className="text-sm">
-              <strong>Control:</strong> {control}
-            </p>
-
-            <p className="text-sm">
-              <strong>Aim down time:</strong> {aimDownTime}
-            </p>
-
-            <p className="text-sm">
-              <strong>Running speed:</strong> {runningSpeed}
-            </p>
-          </div>
-        </CardContent>
-      </Card>
-    )
-  })
-
   return (
     <MotionWrapper
       initial={{ y: '25%', opacity: 0 }}
@@ -267,7 +142,83 @@ export const WeaponryList = () => {
       </div>
 
       <div className="w-full max-w-[100rem] mx-auto flex gap-4 flex-wrap">
-        {weaponCards}
+        {weapons.map((weapon) => {
+          const {
+            id,
+            name,
+            pageName,
+            icon,
+            image,
+            category,
+            firingMode,
+            rangeData,
+            ...rest
+          } = weapon
+
+          const getKeyValuePairsFromStats = Object.entries(rest)
+
+          return (
+            <Card key={weapon.id} className="flex-[calc(33.33%-1rem)]">
+              <CardHeader className="relative">
+                <div className="flex gap-4">
+                  <Link
+                    href={`/weapons/${weapon.pageName
+                      .replace(/\s/g, '-')
+                      .toLowerCase()}`}
+                  >
+                    <Image
+                      src={weapon.icon}
+                      alt={weapon.name}
+                      width={200}
+                      height={100}
+                      quality={100}
+                    />
+                  </Link>
+
+                  <div className="flex flex-col gap-0.5 flex-1">
+                    <CardTitle>{weapon.name}</CardTitle>
+
+                    <CardDescription>{weapon.category}</CardDescription>
+
+                    <CardDescription className="flex">
+                      {weapon.firingMode.join(', ')}
+                    </CardDescription>
+
+                    <div className="flex gap-4">
+                      <Link
+                        className="max-w-fit mt-2 inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-cyan-500 text-primary-foreground shadow hover:bg-cyan-500/80 h-9 px-4 py-2"
+                        href={`/weapons/${weapon.pageName
+                          .replace(/\s/g, '-')
+                          .toLowerCase()}`}
+                      >
+                        Customize
+                      </Link>
+
+                      <Button className="max-w-fit mt-2 inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground shadow hover:bg-primary/90 h-9 px-4 py-2">
+                        Add to Loadout
+                      </Button>
+                    </div>
+                  </div>
+                </div>
+
+                <Separator className="absolute right-0 bottom-0 left-0 bg-white" />
+              </CardHeader>
+
+              {/* Como disponho os parágrafos, de forma a ficarem como antes? Talvez CSS Grid Layout resolva. */}
+              <CardContent className="pt-4 flex gap-2 justify-between">
+                <div className="flex flex-wrap justify-center gap-1">
+                  {getKeyValuePairsFromStats.map(([key, value]) => {
+                    return (
+                      <p key={key} className="text-sm">
+                        <strong className="capitalize">{key}:</strong> {value}
+                      </p>
+                    )
+                  })}
+                </div>
+              </CardContent>
+            </Card>
+          )
+        })}
       </div>
     </MotionWrapper>
   )
